cc81b782bc6d2220f1087a9f88a8313ccdbaceb1
Eric Larson <larson.eric.d@gmail.com>
2012-12-13 16:17:53 -0800

    FIX: More careful polling of topo click

--- mne/viz.py
+++ mne/viz.py
@@ -109,7 +109,7 @@ def _plot_topo_onpick(event, show_func=None, tmin=None, tmax=None,
                       vmin=None, vmax=None, colorbar=False, title=None,
                       x_label=None, y_label=None):
     """Onpick callback that shows a single channel in a new figure"""
-    if event.mouseevent.inaxes is None:
+    if event.mouseevent.inaxes is None or event.mouseevent.button != 1:
         return
     artist = event.artist
     try:
